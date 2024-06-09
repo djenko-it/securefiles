1706373217ddf3165ed6228949d8e7b3127bb744
djenko-it <djenko-it@protonmail.com>
2024-06-09 14:30:00 +0200

    css update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+// tailwind.config.js
 module.exports = {
   content: ['./templates/**/*.html', './static/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out',
+        slideIn: 'slideIn 0.5s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        slideIn: {
+          '0%': { transform: 'translateY(20px)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 }
