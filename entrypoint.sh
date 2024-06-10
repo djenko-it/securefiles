0b139fe08307979d72f6a2b067d06672429683e5
djenko-it <djenko-it@protonmail.com>
2024-06-10 21:46:20 +0200

    data

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@
 # Build the Tailwind CSS
 npm run build:css
 
+# Create the data directory if it doesn't exist
+if [ ! -d "/app/data" ]; then
+  mkdir -p /app/data
+fi
+
 # Initialiser la base de données uniquement si elle n'existe pas
 if [ ! -f "/app/messages.db" ]; then
     python -c "from app import init_db; init_db()"
